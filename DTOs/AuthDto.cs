4c4bb21295499b1e5defc20de2bc85d00c7d378d
Opilane TTHK <opilane@tthk.ee>
2025-12-03 08:46:04 +0200

    Revert "Merge branch 'yl2_#435036' of https://github.com/RipMyLoven/restoran into yl2_#435036"
    This reverts commit 0d35e3147a643122f98dc04d5d1046216b924023, reversing
    changes made to 97be9d9bc42f846d975bd3f017b1bdbfaecff3a6.

--- DTOs/AuthDto.cs
+++ DTOs/AuthDto.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.ComponentModel.DataAnnotations;
 
 namespace Restoran.DTOs
@@ -51,60 +50,3 @@ namespace Restoran.DTOs
         public UserDto User { get; set; } = null!;
     }
 }
-=======
-using System.ComponentModel.DataAnnotations;
-using Restoran.Models;
-
-namespace Restoran.DTOs
-{
-    public class RegisterDto
-    {
-        [Required]
-        [StringLength(50, MinimumLength = 3)]
-        public string Username { get; set; } = string.Empty;
-
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; } = string.Empty;
-
-        [Required]
-        [StringLength(100, MinimumLength = 6)]
-        public string Password { get; set; } = string.Empty;
-
-        [Required]
-        public string FirstName { get; set; } = string.Empty;
-
-        [Required]
-        public string LastName { get; set; } = string.Empty;
-
-        public string PhoneNumber { get; set; } = string.Empty;
-
-        public int? RestaurantId { get; set; }
-
-        public UserRole Role { get; set; } = UserRole.Customer;
-    }
-
-    public class LoginDto
-    {
-        [Required]
-        public string UsernameOrEmail { get; set; } = string.Empty;
-
-        [Required]
-        public string Password { get; set; } = string.Empty;
-    }
-
-    public class RefreshTokenDto
-    {
-        [Required]
-        public string RefreshToken { get; set; } = string.Empty;
-    }
-
-    public class AuthResponseDto
-    {
-        public string AccessToken { get; set; } = string.Empty;
-        public string RefreshToken { get; set; } = string.Empty;
-        public DateTime ExpiresAt { get; set; }
-        public UserDto User { get; set; } = null!;
-    }
-}
->>>>>>> 34174913b0dfb2360f689b5dc65aa2ea5b55f15d
--- DTOs/MenuItemDto.cs
+++ DTOs/MenuItemDto.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.ComponentModel.DataAnnotations;
 
 namespace Restoran.DTOs
@@ -49,56 +48,3 @@ namespace Restoran.DTOs
         public bool? IsAvailable { get; set; }
     }
 }
-=======
-using System.ComponentModel.DataAnnotations;
-
-namespace Restoran.DTOs
-{
-    public class MenuItemDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-        public string Category { get; set; } = string.Empty;
-        public string AllergyInfo { get; set; } = string.Empty;
-        public string DietInfo { get; set; } = string.Empty;
-        public bool IsAvailable { get; set; }
-        public int RestaurantId { get; set; }
-        public DateTime CreatedAt { get; set; }
-    }
-
-    public class CreateMenuItemDto
-    {
-        [Required]
-        [StringLength(100, MinimumLength = 2)]
-        public string Name { get; set; } = string.Empty;
-
-        [StringLength(500)]
-        public string Description { get; set; } = string.Empty;
-
-        [Required]
-        [Range(0.01, 999.99)]
-        public decimal Price { get; set; }
-
-        [Required]
-        public string Category { get; set; } = string.Empty;
-
-        public string AllergyInfo { get; set; } = string.Empty;
-        public string DietInfo { get; set; } = string.Empty;
-        public bool IsAvailable { get; set; } = true;
-        public int RestaurantId { get; set; } = 1; // Default to 1
-    }
-
-    public class UpdateMenuItemDto
-    {
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public decimal? Price { get; set; }
-        public string? Category { get; set; }
-        public string? AllergyInfo { get; set; }
-        public string? DietInfo { get; set; }
-        public bool? IsAvailable { get; set; }
-    }
-}
->>>>>>> 34174913b0dfb2360f689b5dc65aa2ea5b55f15d
